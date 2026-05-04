843e45e4f17e9eb2805dced1127694bc270310c0
LewickiVladyslav <lewickivladyslav@gmail.com>
2026-05-04 17:28:15 +0300

    Update WoodMaterial.hlsl

--- WoodMaterial.hlsl
+++ WoodMaterial.hlsl
@@ -14,7 +14,7 @@ void woodMaterial_float(
     float wet = SAMPLE_TEXTURE2D(Wetness_Mask.tex, Wetness_Mask.samplerstate, UV).r;
     float3 albedo = SAMPLE_TEXTURE2D(Albedo_Tex.tex, Albedo_Tex.samplerstate, UV).rgb;
     float3 normal = SAMPLE_TEXTURE2D(Normal_Tex.tex, Normal_Tex.samplerstate, UV).rgb;
-    float aos = SAMPLE_TEXTURE2D(AOS_Tex.tex, AOS_Tex.samplerstate, UV).rb;
+    float2 aos = SAMPLE_TEXTURE2D(AOS_Tex.tex, AOS_Tex.samplerstate, UV).rb;
 
     
     float3 normalUnpack = normal.xyz * 2.0 - 1.0;
